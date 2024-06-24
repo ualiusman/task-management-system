3c7cd4df830ebec7028e92797e2b453558702b56
ualiusman <usmanaliua.uali@gmail.com>
2024-06-24 14:32:25 +0500

    validation for search and filter

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -12,7 +12,7 @@ export class TasksController {
     }
 
     @Get()
-    getTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
+    getTasks(@Query(ValidationPipe) filterDto: GetTasksFilterDto): Task[] {
         if (Object.keys(filterDto).length) {
             return this.taskService.getTasksWithFilters(filterDto);
         } else {

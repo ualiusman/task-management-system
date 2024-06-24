3a778849a3e6a4c79e83c68200cfd738234a2c4f
ualiusman <usmanaliua.uali@gmail.com>
2024-06-24 12:25:30 +0500

    handling not existing task with NotFoundException

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -29,7 +29,13 @@ export class TasksService {
     }
 
     getTaskById(id: string): Task {
-        return this.tasks.find(x => x.id === id);
+        const found = this.tasks.find(x => x.id === id);
+
+        if (!found) {
+            throw new NotFoundException(`Task with ID "${id} not found" `);
+        }
+
+        return found;
     }
 
     createTask(createTaskDto: CreateTaskDto): Task {

dee2e007b36478f6049d72eedf5a8d254c97cca8
ualiusman <usmanaliua.uali@gmail.com>
2024-07-01 01:34:59 +0500

    commentd code removed

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -32,27 +32,6 @@ export class TasksService {
         const tasks = await query.getMany();
         return tasks;
     }
-    // private tasks: Task[] = [];
-
-    // getAllTasks(): Task[] {
-    //     return this.tasks;
-    // }
-    // getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
-    //     const { status, search } = filterDto;
-    //     let tasks = this.getAllTasks();
-
-    //     if (status) {
-    //         tasks = tasks.filter(task => task.status === status);
-    //     }
-
-    //     if (search) {
-    //         tasks = tasks.filter(task =>
-    //             task.title.includes(search) ||
-    //             task.description.includes(search)
-    //         );
-    //     }
-    //     return tasks;
-    // }
 
     async getTaskById(id: number): Promise<Task> {
         const found = await this.taskRepositoy.findOneBy({ id });

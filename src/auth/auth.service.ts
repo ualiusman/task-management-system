e218fcc8f3398c0b3b1bf806f76a72d576c2bcc8
ualiusman <usmanaliua.uali@gmail.com>
2024-06-30 17:34:24 +0500

    signup completed

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,14 +2,27 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
 import { User } from './user.entity';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @Injectable()
 export class AuthService {
 
 
     constructor(
-        @InjectRepository(User)
-        private taskRepositoy: UserRepository) {
+        @InjectRepository(UserRepository)
+        private userRepositoy: UserRepository) {
+
+    }
+
+
+    async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+
+        const { username, password } = authCredentialsDto;
+
+        const user = new User();
+        user.username = username;
+        user.password = password;
+        await user.save();
 
     }
 }
--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,6 +1,7 @@
 import { Repository } from "typeorm";
 import { InjectRepository } from "@nestjs/typeorm";
 import { User } from "./user.entity";
+import { AuthCredentialsDto } from "./dto/auth-credentials.dto";
 
 export class UserRepository extends Repository<User> {
     constructor(
@@ -8,6 +9,12 @@ export class UserRepository extends Repository<User> {
         private userRepository: Repository<User>,
     ) {
         super(userRepository.target, userRepository.manager, userRepository.queryRunner);
+    }
+
+
+    async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+
+
     }
 
 
